298937868a5a86e413b13280a403d0d68590e7a4
Defi-Crypto <subtorandomstuff@gmail.com>
2023-12-25 17:38:53 -0600

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                {/* <img src={logo} className="App-logo" alt="logo" /> */}
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a></header>
-            <nav><a href="/home">Home</a> | <a href="/projects">Projects</a> | <a href="/about">About</a> | <a href="/contact">Contact Us</a></nav>
-        </div>
+        
+        
+        
+       <>
+        
+        <Header/>
+        <h1 className="text-3xl font-bold  App">
+      Hello world!
+        </h1>
+        </>
     );
 }
 
